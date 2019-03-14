929b172df6e69481f65d208a346295864dd7b2c0
David Langheiter <david@langheiter.com>
2019-03-14 12:25:23 +0100

    first auth implementation

--- src/main/java/decorator/textreader/Authentication.java
+++ src/main/java/decorator/textreader/Authentication.java
@@ -1,9 +1,22 @@
 package decorator.textreader;
 
+import java.io.IOException;
+
 public class Authentication extends Decorator {
+	private TextReader textReader;
+
+	public Authentication(TextReader textReader) {
+		this.textReader = textReader;
+	}
 
 	public void write(String[] s) {
-
+		System.out.print("PASSWORD:\t\t");
+		try {
+			s[0] = in.readLine();
+			textReader.write(s);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 	}
 
 	public void read(String[] s) {
