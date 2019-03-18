36039a69bb0ba98a9d192216c9015e9652f4d6f5
David Langheiter <david@langheiter.com>
2019-03-18 15:02:13 +0100

    Moved Encryption to own class

--- README.md
+++ README.md
@@ -6,6 +6,14 @@ Die detaillierte [Aufgabenstellung](TASK.md) beschreibt die notwendigen Schritte
 ## Implementierung
 
 
+#### Move TextReader to Interface
+Vorteile:
+* Es ist Universeller
+* Keine unötigen sachen die villeicht nicht gebraucht werden
+
+Nachteile:
+* Man muss den `BufferedReader in` in jeder Klasse definieren in dem er gebraucht wird.
+Auch wenn man ihn davor nur einmal definieren musse
 
 ## Quellen
 * [AES](https://howtodoinjava.com/security/aes-256-encryption-decryption/)
--- src/main/java/decorator/textreader/Scrambling.java
+++ src/main/java/decorator/textreader/Scrambling.java
@@ -16,7 +16,6 @@ import java.util.Base64;
  */
 public class Scrambling extends Decorator {
     private TextReader textReader;
-    private static String salt = "ssshhhhhhhhhhh!!!!";
 
     public Scrambling(TextReader textReader) {
         this.textReader = textReader;
@@ -29,11 +28,11 @@ public class Scrambling extends Decorator {
 
         System.out.println("encrypt:\t\t");
 
-        s[0] = encrypt(got[0], s[0]);
+        s[0] = Encryption.encrypt(got[0], s[0]);
     }
 
     public void read(String[] s) {
-        String out = decrypt(s[0], s[1]);
+        String out = Encryption.decrypt(s[0], s[1]);
         if (out == null) {
             System.out.print("ERROR:\t\tWrong password");
             return;
@@ -42,42 +41,6 @@ public class Scrambling extends Decorator {
         textReader.read(new String[]{out});
     }
 
-    public String encrypt(String input, String password) {
-        try {
-            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-            IvParameterSpec ivspec = new IvParameterSpec(iv);
 
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
-            SecretKey tmp = factory.generateSecret(spec);
-            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(input.getBytes("UTF-8")));
-        } catch (Exception e) {
-            //e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static String decrypt(String input, String password) {
-        try {
-            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-            IvParameterSpec ivspec = new IvParameterSpec(iv);
-
-            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-            KeySpec spec = new PBEKeySpec(password.toCharArray(), salt.getBytes(), 65536, 256);
-            SecretKey tmp = factory.generateSecret(spec);
-            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
-            return new String(cipher.doFinal(Base64.getDecoder().decode(input)));
-        } catch (Exception e) {
-            //e.printStackTrace();
-        }
-        return null;
-    }
 
 }
